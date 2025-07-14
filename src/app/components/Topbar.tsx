d0e944e462e227d2bf1e3f7018b828e8a892ffbd
ryanyychen <ryan_yychen@hotmail.com>
2025-07-14 10:26:01 +0800

    menu bar animations

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+'use client';
 
-const Topbar: React.FC = () => (
-    <nav className="flex items-center justify-center md:justify-end px-8 py-2 md:pr-14 bg-secondary opacity-[80%] text-primary h-[10vh] w-full">
-        <ul className="flex gap-6 list-none m-0 p-0">
-            <li>
-                <a href="/" className="text-lg font-bold inline-block transition-all duration-200 hover:scale-[1.2]">Home</a>
-            </li>
-            <li>
-                <a href="/about" className="text-lg font-bold inline-block transition-all duration-200 hover:scale-[1.2]">About</a>
-            </li>
-            <li>
-                <a href="/projects" className="text-lg font-bold inline-block transition-all duration-200 hover:scale-[1.2]">Projects</a>
-            </li>
-        </ul>
-    </nav>
-);
+import React from "react";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+    { href: "/", label: "Home", id: "home"},
+    { href: "/about", label: "About", id: "about" },
+    { href: "/projects", label: "Projects", id: "projects" },
+    { href: "/resume", label: "Resume", id: "resume" },
+];
+
+const Topbar: React.FC = () => {
+    const pathname = usePathname();
+
+    return (
+        <nav className="flex items-center justify-center md:justify-end px-8 py-2 md:pr-14 bg-primary text-secondary h-[10vh] w-full">
+            <ul className="flex gap-6 list-none m-0 p-0">
+                {navLinks.map(link => (
+                    <li key={link.id}>
+                        <a href={link.href} id={link.id} className={`group relative text-lg font-bold inline-block transition-all duration-200 hover:scale-[1.2] ${pathname === link.href ? "animated-underline" : ""}`}>
+                            {link.label}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+};
 
 export default Topbar;
