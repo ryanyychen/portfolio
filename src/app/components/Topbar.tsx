7aecffea34fc141bc0a86c1630d2a6b027616410
ryanyychen <ryan_yychen@hotmail.com>
2025-07-17 10:18:20 +0800

    Update navbar links

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from "react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const navLinks = [
@@ -17,11 +18,9 @@ const Topbar: React.FC = () => {
         <nav className="flex items-center justify-center md:justify-end px-8 py-2 md:pr-14 bg-primary text-secondary h-[10vh] w-full">
             <ul className="flex gap-6 list-none m-0 p-0">
                 {navLinks.map(link => (
-                    <li key={link.id}>
-                        <a href={link.href} id={link.id} className={`group relative text-lg font-bold inline-block transition-all duration-200 hover:scale-[1.2] ${pathname === link.href ? "animated-underline" : ""}`}>
-                            {link.label}
-                        </a>
-                    </li>
+                    <Link key={link.id} href={link.href} className={`group relative text-lg font-bold inline-block transition-all duration-200 hover:scale-[1.2] ${pathname === link.href ? "animated-underline" : ""}`}>
+                        {link.label}
+                    </Link>
                 ))}
             </ul>
         </nav>
