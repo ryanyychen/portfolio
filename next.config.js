6e3ef3b82e785ed914d6affdbbf0930aa735d8cd
ryanyychen <ryan_yychen@hotmail.com>
2025-07-17 11:14:27 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isGitHubPages = process.env.NEXT_PUBLIC_BASE_PATH === "/personal-website";
 console.log("isGitHubPages:", isGitHubPages);
 
 const nextConfig = {
-  output: isGitHubPages ? "export" : "",
+  output: isGitHubPages ? "export" : "standalone",
   basePath: isGitHubPages ? "/personal-website" : "",
   assetPrefix: isGitHubPages ? "/personal-website/" : "",
 };
