a1b9a0b279e2fdc4cf98a5d0e20621a257ee60a1
ryanyychen <ryan_yychen@hotmail.com>
2025-07-26 17:48:27 +0800

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,22 +5,21 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const navLinks = [
-    { href: "/", label: "Home", id: "home"},
     { href: "/resume", label: "Resume", id: "resume" },
 ];
 
 const Navbar: React.FC = () => {
     const pathname = usePathname();
+    let resumeActive = pathname === "/resume";
 
     return (
         <nav className="flex items-center justify-between px-8 py-2 bg-primary text-secondary h-[10vh] w-full">
-            <p className="text-3xl font-bold font-nasalization justify-start">Ryan Chen</p>
+            <p className="text-3xl font-bold font-audiowide justify-start">Ryan Chen</p>
             <ul className="flex gap-6 list-none m-0 p-0">
-                {navLinks.map(link => (
-                    <Link key={link.id} href={link.href} className={`group relative text-lg font-bold inline-block transition-all duration-200 hover:scale-[1.2] ${pathname === link.href ? "animated-underline" : ""}`}>
-                        {link.label}
-                    </Link>
-                ))}
+                {resumeActive
+                    ? <Link href="/"><button className="homebutton">Home</button></Link>
+                    : <Link href="/resume"><button className="homebutton">Resume</button></Link>
+                }
             </ul>
         </nav>
     )
