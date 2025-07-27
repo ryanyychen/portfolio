075489e4860e74a6b600c08c7e38f1753adfe570
ryanyychen <ryan_yychen@hotmail.com>
2025-07-27 11:08:26 +0800

    Add education

---
+++ src/app/components/Education.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import { prefix } from '@/app/prefix';
+import { useIsSmallScreen } from '@/app/utils';
+
+const Education: React.FC = () => {
+    const smallScreen = useIsSmallScreen();
+    let usc = smallScreen ? "Southern Cal" : "University of California, San Diego";
+    let masters = smallScreen ? "M.S. CS" : "Master of Science, Computer Science";
+    let ucsd = smallScreen ? "UC San Diego" : "University of Southern California";
+    let bachelors = smallScreen ? "B.S. Math-CS" : "Bachelor of Science, Mathematics-Computer Science";
+    return (
+        <div className="flex flex-col items-center justify-center w-full gap-4">
+            <h1 className="section-title">Education</h1>
+            <div className="flex flex-row items-center gap-4">
+                <Image src={`${prefix}/usc.svg`} alt="USC Logo" width={100} height={100} className="h-[10vh] md:h-[15vh] w-auto" />
+                <div className="flex flex-col items-start w-[45vw] md:w-[40vw]">
+                    <h2 className="text-2xl md:text-3xl">{usc}</h2>
+                    <h3 className="text-xl font-quicksand">{masters}</h3>
+                </div>
+            </div>
+            <div className="flex flex-row items-center gap-4 mb-[20vh]">
+                <Image src={`${prefix}/ucsd.svg`} alt="UCSD Logo" width={100} height={100} className="h-[10vh] md:h-[15vh] w-auto" />
+                <div className="flex flex-col items-start w-[45vw] md:w-[40vw]">
+                    <h2 className="text-2xl md:text-3xl">{ucsd}</h2>
+                    <h3 className="text-xl font-quicksand">{bachelors}</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Education;
