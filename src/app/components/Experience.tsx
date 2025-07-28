07f59291cfe4d92f98a014419bbaaae1d3d61fa9
ryanyychen <ryan_yychen@hotmail.com>
2025-07-28 11:13:29 +0800

    Update experience section

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,11 +1,13 @@
 import React, { useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
+import { useIsSmallScreen } from '@/app/utils';
 
 interface ExperienceProps {
     experiences?: Array<{position: string; company: string; start: string; end: string; description: string;}>;
 }
 
 const Experience: React.FC<ExperienceProps> = ({ experiences = [] }) => {
+    const smallScreen = useIsSmallScreen();
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true, margin: "0px 0px -100px 0px" });
 
@@ -14,23 +16,22 @@ const Experience: React.FC<ExperienceProps> = ({ experiences = [] }) => {
             <h1 className="section-title">Experience</h1>
             {experiences.map((experience, idx) => {
                 return (
-                    <div key={idx} className="flex flex-row w-[80vw] gap-8">
+                    <div key={idx} className="flex flex-col md:flex-row w-[80vw] justify-between">
                         <motion.div
                             initial={{ opacity: 0, x: -50 }}
                             animate={isInView ? { opacity: 1, x: 0 } : {}}
-                            transition={{ duration: 1.5, delay: idx * 0.2 }}
-                            className="w-[1/4]">
-                            <h2 className="text-lg font-quicksand">{experience.start}-{experience.end}</h2>
+                            transition={{ duration: 1.5, delay: smallScreen ? idx * 0.4 : idx * 0.2 }}
+                            className="w-full md:w-[30vw]">
+                            <h2 className="text-lg text-start md:text-center font-quicksand">{experience.start} &mdash; {experience.end}</h2>
                         </motion.div>
                         <motion.div
                             initial={{ opacity: 0, x: 50 }}
                             animate={isInView ? { opacity: 1, x: 0 } : {}}
-                            transition={{ duration: 1.5, delay: idx * 0.2 + 0.2 }}
-                            className="w-[3/4]">
-                            <span className="flex flex-row items-end">
-                                <h2 className="text-2xl font-bold">{experience.position}</h2>
-                                <h2 className="text-xl font-quicksand">, {experience.company}</h2>
-                            </span>
+                            transition={{ duration: 1.5, delay: smallScreen ? idx * 0.4 + 0.2 : idx * 0.2 + 0.2 }}
+                            className="w-full mb-4">
+                            <h2 className="text-2xl font-bold">{experience.position}</h2>
+                            <h2 className="text-lg font-quicksand">{experience.company}</h2>
+                            <p className="mt-2 text-md">{experience.description}</p>
                         </motion.div>
                     </div>
                 )
