8e9d11c6f14cecac6975c0db94246c510c4cdb1d
Ryan Chen <ryan_yychen@hotmail.com>
2025-08-21 22:31:29 -0700

    Update handling of wave color

--- src/app/components/Waves.tsx
+++ src/app/components/Waves.tsx
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-let color = "#212121"
-const theme = localStorage.getItem("theme") || "light";
-if (theme === "light") {
-    color = "#212121"
-} else {
-    color = "#a4a4a4"
-}
 
 const Waves: React.FC = () => {
     const [showFooter, setShowFooter] = useState(false);
@@ -27,21 +20,18 @@ const Waves: React.FC = () => {
     return (
         <div className="w-full absolute bottom-0 left-0 z-10 pointer-events-none">
             <div className="relative w-full h-48 md:h-64">
-                <svg className="absolute inset-0 w-full h-full" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                <svg className="absolute inset-0 w-full h-full fill-wave" viewBox="0 0 1440 320" preserveAspectRatio="none">
                     <path
-                        fill={color}
                         fillOpacity="0.4"
                         d="M0,200 C480,250 960,50 1440,200 L1440,320 L0,320 Z"
                     />
 
                     <path
-                        fill={color}
                         fillOpacity="0.6"
                         d="M0,220 C480,100 960,270 1440,220 L1440,320 L0,320 Z"
                     />
 
                     <path
-                        fill={color}
                         fillOpacity="1"
                         d="M0,240 C360,350 1080,140 1440,260 L1440,320 L0,320 Z"
                     />
